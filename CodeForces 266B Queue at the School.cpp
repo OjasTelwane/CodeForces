f9af2e9d4b9c93ee84afb07799cb681578387396
Ojas S. Telwane <48923681+OjasTelwane@users.noreply.github.com>
2021-11-10 11:28:10 +0530

    Add files via upload

---
+++ CodeForces 266B Queue at the School.cpp
@@ -0,0 +1,18 @@
+// CodeForces 266B Queue at the School
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, b;
+string a;
+int main() {
+    cin >> n >> b >> a;
+    for (int i = 0; i < b; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[j] == 'B' && a[j + 1] == 'G') {
+                swap(a[j], a[j + 1]);
+                j += 1;
+            }
+        }
+    }
+    cout << a;
+}

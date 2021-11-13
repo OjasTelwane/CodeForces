a3b65e2c43593ee4c9cc4e534d039a416a290c0b
Ojas S. Telwane <48923681+OjasTelwane@users.noreply.github.com>
2021-11-13 07:57:04 +0530

    Add files via upload

---
+++ CodeForces 791A Bear and Big Brother.cpp
@@ -0,0 +1,14 @@
+// CodeForces 791A Bear and Big Brother
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a, b, count=0;
+    cin>>a>>b;
+    while(a<=b){
+        a=a*3;
+        b=b*2;
+        count++;
+    }
+    cout<<count;
+    return 0;
+}
